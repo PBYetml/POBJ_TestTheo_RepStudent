c466e4ae6efb1e2b5a06f4549303ac3490a83e59
PBYetml <philippe.bovey@eduvaud.ch>
2023-05-09 18:12:54 +0200

    MAJ dossier MSS

---
+++ MSS/TEST2_27-04-23_MSS/MSS_TEST2_Question5.cs
@@ -0,0 +1,26 @@
+// -- TEST2 SLO QUESTION 5 --//
+// -- Miguel Santos --//
+
+// Chemin d'accès :
+// O:\APPLICATIONS\Echanges_Eleves\ES\SLO2\Santos_Miguel\Epreuves\POBJ\TEST2_27-04-23_MSS
+
+// Initialisation variable numérique 16 bits
+char ValNumUnicode = '\u0031';
+
+// Structure de méthode privée "Calculer Echantillon"
+private void CalculerEchantillon(ref sbyte Amp, ref double[] tbEchantillon)
+{
+	
+}
+
+// Complément à 1 d'une variable
+uint valX = 0xFF00FF00;
+valX = ~valX;
+
+// Itération d'un tableau
+char[] tbChar = new char[4] { 'a', 'b', 'c', 'd'};
+uint i;
+for (i = 0; i < tbChar.Length; i++)
+{
+	System.Console.WriteLine(tbChar[i]);
+}
---
+++ MSS/TEST2_27-04-23_MSS/Test2SLO_MSS/Form1.cs
@@ -0,0 +1,54 @@
+﻿// -- TEST2 SLO QUESTION 3 --//
+// -- Miguel Santos --//
+
+// Chemin d'accès :
+// O:\APPLICATIONS\Echanges_Eleves\ES\SLO2\Santos_Miguel\Epreuves\POBJ\TEST2_27-04-23_MSS
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test2SLO_MSS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rbSignalSinus.Text = "Sinus";
+            rbSignalSinus.Checked = false;
+
+            rbSignalTTL.Text = "TTL";
+            rbSignalTTL.Checked = true;
+
+            cbOffset.Text = "Offset";
+            cbOffset.Enabled = rbSignalSinus.Checked;
+        }
+
+        private void rbSignalSinus_CheckedChanged(object sender, EventArgs e)
+        {
+            cbOffset.Enabled = rbSignalSinus.Checked;
+        }
+
+        public void CalculMoyennePOBJ(ref int nbrNoteMax, ref float [] ptTbNote, out float moyenne)
+        {
+            //-- Déclaration variable --//
+            int increment;
+            float sommeNote = 0;
+            for (increment = 0; increment < nbrNoteMax; increment++)
+                sommeNote += ptTbNote[increment];
+
+            moyenne = sommeNote / nbrNoteMax;
+        }
+    }
+}
